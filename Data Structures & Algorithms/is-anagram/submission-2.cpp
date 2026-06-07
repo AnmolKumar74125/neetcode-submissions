2fc7ff21a5347a2c8aff915b3f3ac851c4ea986a
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2026-06-08 01:07:51 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+        int arr[26];
+        fill(arr,arr+26,0);
+        for(int i = 0; i<s.length(); i++)
+        {
+            arr[s[i]-97]++;
+            arr[t[i]-97]--;
+        }
+        for(int i = 0; i< 26;i++)
+        {
+            if(arr[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
