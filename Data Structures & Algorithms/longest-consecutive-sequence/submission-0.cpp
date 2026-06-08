2eacbd21d25224cfb46a306c7cf7744a2bb29e27
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2026-06-09 01:54:00 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        unordered_map<int,int> hm;
+        int ans = 0;
+        for(int i= 0; i < (int)nums.size(); i++)
+        {
+            if(hm.count(nums[i]-1) > 0)
+            {
+                hm[nums[i]] = hm[nums[i]-1]+1;
+            }
+            else
+            {
+                hm[nums[i]] = 1;
+            }
+            ans = max(ans, hm[nums[i]]);
+            cout<<nums[i]<<" -> "<<hm[nums[i]]<<endl;
+        }
+        return ans;
+    }
+};
