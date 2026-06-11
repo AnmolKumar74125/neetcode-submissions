90bf073305e4ac488ddb77e102823aed8443eba4
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2026-06-12 02:01:25 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+       int ans = 0;
+       for(int i = s.length()-1; i>=0; i--)
+       {
+            if(s[i] == ' ')
+            {
+                if(ans == 0)
+                {
+                    continue;
+                }
+                break;
+            }
+            ans++;
+       }
+       return ans;
+    }
+};
