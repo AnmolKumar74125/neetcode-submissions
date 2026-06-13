0a01f76558278d776047774f340be800b6a5d93c
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2026-06-14 04:00:44 +0530

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        k = k % n;
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n-1);
+    }
+    void reverse(vector<int>& nums, int a, int b)
+    {
+        while(a<b)
+        {
+            int temp = nums[a];
+            nums[a++] = nums[b];
+            nums[b--] = temp;
+        }
+    }
+};
