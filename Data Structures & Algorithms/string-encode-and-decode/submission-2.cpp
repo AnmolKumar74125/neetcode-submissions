79f0da1f70898d7f4fdc2f8c845e505dffc275be
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2026-06-20 17:56:21 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> len;
+    string encode(vector<string>& strs) {
+        string s = "";
+        for(int i = 0; i < strs.size(); i++)
+        {
+            len.push_back(strs[i].length());
+            s = s + strs[i];
+        }
+        return s;
+    }
+
+    vector<string> decode(string s) {
+
+        vector<string> ans;
+        int x = 0;
+        for(int i = 0; i < len.size(); i++)
+        {
+            ans.push_back(s.substr(x,len[i]));
+            x = x + len[i];
+        }
+        return ans;
+    }
+};
